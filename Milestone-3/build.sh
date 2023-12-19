a8a76b7343d581ec27eeb83648684d0c59c6723d
nikhilreddy3888 <nikhilreddy3888@gmail.com>
2023-12-19 14:45:14 -0500

    Milestone-3

---
+++ Milestone-3/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "TODO: fill in the docker build command"
+
+#!/bin/bash
+docker build --no-cache -t app:latest -f Dockerfile.serving . 
---
+++ Milestone-3/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "TODO: fill in the docker run command"
+
+#!/bin/bash
+docker run -p 8000:8000 -e COMET_API_KEY=${COMET_API_KEY} app:latest
